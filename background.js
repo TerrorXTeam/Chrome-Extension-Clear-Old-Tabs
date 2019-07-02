1f0a3982ab82f3c75d00e274b9b7198e608c153e
TerrorXTeam <36647590+TerrorXTeam@users.noreply.github.com>
2019-07-02 07:41:45 +0200

    Add files via upload

---
+++ background.js
@@ -0,0 +1,67 @@
+console.log("bckg works");
+
+var tabsAndTimes=[];
+
+chrome.tabs.query({},function(tabs){
+    for (tab of tabs){
+        //console.log(tab.title)
+    }
+});
+
+//Event listener which adds/updates an array of objects.
+//Objects=dictionary with tabID,TabName,LastClicked time
+chrome.tabs.onHighlighted.addListener(function tabID(tab){
+    reqTab=Number(tab.tabIds)
+    chrome.tabs.get(reqTab,function returnName(Tab){
+       tabTitle=Tab.title;
+       timeStampClick= new Date();
+       for (timeStamps of tabsAndTimes){
+       if(reqTab === timeStamps["TabID"]){
+           timeStamps["TabName"]=tabTitle
+           timeStamps["LastClicked"]=timeStampClick
+           var STOP="Yes"
+       }}
+       if(STOP!="Yes"){
+       tabsAndTimes.push({
+            TabID:reqTab,
+            TabName:tabTitle,
+            LastClicked:timeStampClick
+       })}
+       console.log(tabsAndTimes)
+    })
+
+})
+//Event listener which removes objects from the tabsAndTimes array when a matching tab is closed
+chrome.tabs.onRemoved.addListener(function XtabID(tabID){
+    for(var i=0; i<tabsAndTimes.length; i++){
+        if (tabsAndTimes[i].TabID === tabID){
+            tabsAndTimes.splice(i,1)
+        }
+    }
+})
+
+//Sorts objects in an array
+function sortObjects(a,b){
+    const aClicked=a.LastClicked;
+    const bClicked=b.LastClicked;
+
+    let comparison=0;
+    if (aClicked > bClicked){
+        comparison=1;
+    }
+    else if (aClicked < bClicked){
+        comparison=-1;
+    }
+    return comparison;
+};
+
+
+
+
+chrome.browserAction.onClicked.addListener(buttonClicked)
+
+function buttonClicked(Tab){
+    tabsAndTimes.sort(sortObjects)
+
+    console.log(tabsAndTimes)
+} 
