96c600aba55d7e49a68b89a59748148423f06251
TerrorXTeam <36647590+TerrorXTeam@users.noreply.github.com>
2019-07-03 21:12:13 +0200

    Update popup.js

--- views/js/popup.js
+++ views/js/popup.js
@@ -1,11 +1,13 @@
 
     chrome.runtime.sendMessage({popupOpen: true}, function(response) {
+       currentDate= new Date()
       /* process response */
       console.log(response)
       for(obj of response){
+         deltaClickTime=currentDate-obj.LastClicked;
         $('#example tbody').append('<tr> <td> </td> <td>'+obj.TabName+'</td>'+
-        '<td> </td> <td>'+obj.LastClicked+'</td></tr>')
-        console.log(obj.TabName+" "+obj.LastClicked)
+        '<td> </td> <td>'+deltaClickTime+'</td></tr>')
+        console.log(obj.TabName+" "+formatDate(obj.LastClicked)+"   "+ deltaClickTime);
       };
       var table = $('#example').DataTable({
          'columnDefs': [
@@ -21,3 +23,19 @@
          }
       });
     });
+
+
+
+    function formatDate(date){
+
+      //var dd = date.getDate();
+      var mm = date.getMonth()+1;
+      var yyyy = date.getFullYear();
+      var hh = date.getHours();
+      var ss = date.getSeconds();
+      if(dd<10) {dd='0'+dd}
+      if(mm<10) {mm='0'+mm}
+      date = yyyy+mm+dd;
+      time=hh+ss;
+      return time
+   }
