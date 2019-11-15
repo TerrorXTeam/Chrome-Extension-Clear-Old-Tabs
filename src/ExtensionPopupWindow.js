d37e7ace1ef346b38a6df19cdbdb6845d7efe9d6
TerrorXTeam <36647590+TerrorXTeam@users.noreply.github.com>
2019-11-15 17:34:58 +0100

    Created Component Structure

---
+++ src/ExtensionPopupWindow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from 'ExtensionPopupWindow.js' 
+
+class ExtensionPopupWindow extends Component  {
+  render () {
+  return (
+    <div className="App">
+  Add your components here
+    </div>
+  );
+}
+}
+export default ExtensionPopupWindow ;
