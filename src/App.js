f20ceccb9190764d65f1b9f8db21610b7b1658ab
coder-A <alexanderterziev199@gmail.com>
2019-11-15 06:51:39 +0100

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div><h1>This is will be a chrome extension</h1>
+    <Button variant='contained' color='primary'>
+    Hellow World
+    </Button>
     </div>
   );
 }
