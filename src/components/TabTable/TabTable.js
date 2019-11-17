bea412af162821a50b85bdb9aee06ae3abdf408b
36647590 <alexanderterziev199@gmail.com>
2019-11-17 13:02:27 +0100

    Fron-end ready

--- src/components/TabTable/TabTable.js
+++ src/components/TabTable/TabTable.js
@@ -5,23 +5,58 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Checkbox from '@material-ui/core/Checkbox';
+import Fab from '@material-ui/core/Fab';
+import CloseIcon from '@material-ui/icons/Close';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+
+
+const StyleTableCell = withStyles(theme => ({
+    paddingCheckbox:{
+        padding:30,
+    },
+    head: {
+        backgroundColor:"#009879",
+        color: "#ffffff",
+        textAlign: "left",
+        "font-weight": "bold"
+    },
+    body:{
+        "border-bottom": "1px solid #dddddd",
+        fontSize: 14,
+    }
+}))(TableCell);
+
+
+const StyleTableRow=withStyles(({
+    root:{
+
+        '&:nth-of-type(odd)':{
+            backgroundColor: "#f3f3f3f1"
+        },
+    
+    },
+}))(TableRow);
+
 
 
 class TabTable extends Component {
+
+
     render() {
+
         const rows = [];
 
         this.props.chromeTabs.forEach((tab) => {
             rows.push(
-                <TableRow key={tab.tabID}>
-                    <TableCell align="left">
+                <StyleTableRow key={tab.tabID}>
+                    <StyleTableCell padding="checkbox"  size="small"  align="left">
                         <Checkbox />
-                    </TableCell>
-                    <TableCell component="th" scrope="row">
+                    </StyleTableCell>
+                    <StyleTableCell  size="small"  component="th" scrope="row">
                         {tab.tabName}
-                    </TableCell>
-                    <TableCell align="left">{tab.lastTimeClicked}</TableCell>
-                </TableRow>
+                    </StyleTableCell>
+                    <StyleTableCell  size="small"  align="left">{tab.lastTimeClicked}</StyleTableCell>
+                </StyleTableRow>
             )
 
         })
@@ -30,15 +65,21 @@ class TabTable extends Component {
                 <Table size="small" aria-label="a dense table">
                     <TableHead>
                         <TableRow>
-                            <TableCell />
-                            <TableCell align="left">Tab Name</TableCell>
-                            <TableCell align="left">Last Time Used</TableCell>
+                            <StyleTableCell padding="checkbox" />
+                            <StyleTableCell align="left">Tab Name</StyleTableCell>
+                            <StyleTableCell align="left">Last Time Used</StyleTableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {rows}
                     </TableBody>
                 </Table>
+                <div>
+                    <Fab color="primary" aria-lable="close" size="small" disabled={false}>
+                        <CloseIcon />
+                    </Fab>
+                </div>
+
             </div>
         );
     }
