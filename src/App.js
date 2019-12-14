d58059257f97277f468e7c16cf2934a8266c672d
36647590 <alexanderterziev199@gmail.com>
2019-12-14 15:57:13 +0100

    will learn react

--- src/App.js
+++ src/App.js
@@ -1,51 +1,58 @@
 import React, { Component } from "react";
 import styles from "./App.css";
 import TabTable from "./components/TabTable/TabTable";
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       ChromeTabsData: null,
-      tabsToClose: []
+      tabsToClose: [],
+      value:0
     };
     this.closeTabsHandler = this.closeTabsHandler.bind(this);
     this.fetchStorageDataHandler = this.fetchStorageDataHandler.bind(this);
     this.chromeCloseTabsHandler = this.chromeCloseTabsHandler.bind(this);
-    this.sortObjects=this.sortObjects.bind(this);
+    this.sortObjects = this.sortObjects.bind(this);
   }
 
-//Sorts objects in an array
-  sortObjects=(a,b)=>{
+  //Sorts objects in an array
+  sortObjects = (a, b) => {
     console.log(a)
     console.log(b)
-     const aClicked=new Date(a.timeStampClick);
-     const bClicked=new Date(b.timeStampClick);
+    const aClicked = new Date(a.timeStampClick);
+    const bClicked = new Date(b.timeStampClick);
 
-    let comparison=0;
-    if (aClicked > bClicked){
-        comparison=1;
+    let comparison = 0;
+    if (aClicked > bClicked) {
+      comparison = 1;
     }
-    else if (aClicked < bClicked){
-        comparison=-1;
+    else if (aClicked < bClicked) {
+      comparison = -1;
     }
     return comparison;
-};
+  };
 
-//Get tabs stats from local storage and refresh the state
+  //Get tabs stats from local storage and refresh the state
   fetchStorageDataHandler = () => {
     window.chrome.storage.local.get(
       null,
       function (data) {
         console.log(Object.values(data))
-        const sortedTabsData=Object.values(data).sort(this.sortObjects)
+        const sortedTabsData = Object.values(data).sort(this.sortObjects)
         this.setState({
           ChromeTabsData: sortedTabsData//Object.values(data)
         })
       }.bind(this)
     );
   }
-//Close the selected chrome tabs 
+  //Close the selected chrome tabs 
   chromeCloseTabsHandler = (tabs) => {
     let chromeCloseList = [];
     tabs.forEach((tab) => {
@@ -54,7 +61,7 @@ class App extends Component {
     })
     window.chrome.tabs.remove(chromeCloseList)
     this.setState({
-      tabsToClose:[]
+      tabsToClose: []
     })
   }
 
@@ -63,7 +70,7 @@ class App extends Component {
     this.fetchStorageDataHandler();
   }
 
-//Update state and remove closed tabs from local storage
+  //Update state and remove closed tabs from local storage
   closeTabsHandler() {
     window.chrome.storage.local.remove(this.state.tabsToClose, function () {
       this.fetchStorageDataHandler();
@@ -75,7 +82,26 @@ class App extends Component {
     return (
       this.state.ChromeTabsData && (
         <div className={styles.App}>
-          <TabTable chromeTabs={this.state.ChromeTabsData} closeTabs={this.closeTabsHandler} ToClose={this.state.tabsToClose} currentTime={new Date()} />
+          <TabTable
+            chromeTabs={this.state.ChromeTabsData}
+            closeTabs={this.closeTabsHandler}
+            ToClose={this.state.tabsToClose}
+            currentTime={new Date()} />
+
+          <BottomNavigation
+            value={this.state.value}
+            onChange={(event, newValue) => {
+              this.setState({value : newValue});
+            }}
+            showLabels
+          // className={classes.root}
+          >
+            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+          </BottomNavigation>
+
+
         </div>
       )
     );
---
+++ src/components/RecentlyClosedTabs/ClosedTabs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'
+const ClosedTabs = (props) => (
+<Aux>
+    <div>
+        <h1>CLosed Tabs History is here</h1>
+    </div>
+</Aux>
+)
+export default ClosedTabs;
